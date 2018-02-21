cea4c269c860e3721605f4f1ec0d6bd313a4b8f1
Roberto C Navarro <rnavarro70@gmail.com>
2018-02-21 11:02:58 -0600

    initial commit

---
+++ WorkbookSolutionCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkbookSolutionCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
